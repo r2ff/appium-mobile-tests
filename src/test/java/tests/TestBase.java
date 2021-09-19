0e055936d3103717edd9fc58ad11e6cf85360165
Alexey <reni.gate@gmail.com>
2021-09-19 18:55:51 +0500

    formatting lines

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -57,9 +57,7 @@ public class TestBase {
 //        Attach.screenshotAs("Last screenshot");
 //        Attach.pageSource();
 ////        Attach.browserConsoleLogs();
-
         closeWebDriver();
-
 //        Attach.attachVideo(sessionId);
 
     }
